33c6467f8669e5ac5d521b7b4fa0228f5189abe8
Matteo Di Noia <matteodinoia02@gmail.com>
2025-03-29 19:17:11 +0100

    much more refactoring

--- 01/main-es2.py
+++ 01/main-es2.py
@@ -2,36 +2,32 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt, ceil, floor
-from random import choices
+
+RANGE = 1000000
 
 if __name__ == "__main__":
-    extracted_exp: list = []
-    extracted_uni: list = []
-    rng: np.random.Generator = np.random.default_rng()
+    vals_exp: list = []
+    vals_uni: list = []
+    rng = np.random.default_rng()
 
-    RANGE = 1000000
     for _ in range(RANGE):
-        extracted_exp.append(
+        vals_exp.append(
             rng.exponential(1)
         )
-        extracted_uni.append(
+        vals_uni.append(
             rng.uniform(0, 5)
         )
 
-    greater = sum(
-        1
-        for i in range(RANGE)
-        if extracted_exp[i] > extracted_uni[i]
-    )
 
-    print(np.mean(extracted_exp))
-    print(np.mean(extracted_uni))
+
+    print(np.mean(vals_exp))
+    print(np.mean(vals_uni))
+    greater = sum(1 for i in range(RANGE) if vals_exp[i] > vals_uni[i])
     print(greater / RANGE)
 
     fig, ax = plt.subplots()
-    ax.hist(extracted_exp)
-    ax.hist(extracted_uni)
+    ax.hist(vals_exp, linewidth=0.5, edgecolor="white", bins=40)
+    ax.hist(vals_uni, linewidth=0.5, edgecolor="white", bins=40)
     plt.show()
 
 
--- 01/main.py
+++ 01/main.py
@@ -2,39 +2,40 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from math import sqrt, ceil, floor
 from random import choices
 
+RANGE = 10000000
+
 if __name__ == "__main__":
-    random_values: list = [(-2, sqrt(2)), (4, 1), (10, sqrt(3)), (15, sqrt(2))]
-    probabilities: list = [0.15, 0.25, 0.35, 0.25]
-    extracted_values: list = []
-    rng: np.random.Generator = np.random.default_rng()
-    for _ in range(1000000):
-        mu, sigma = choices(
-            random_values, probabilities, k=1
-        )[0]
-        extracted_values.append(
-            rng.normal(mu, sigma, 1)[0]
-        )
-    print(np.mean(extracted_values))
-    print(np.var(extracted_values))
+    choice_mean = [-2, 4, 10, 15]
+    choice_var = [2, 1, 3, 2]
+    choice_prob = [0.15, 0.25, 0.35, 0.25]
+    vals = []
+    rng = np.random.default_rng()
 
-    # Calculate expected
+
     print("EXPECTED ARE:")
-    expected_mean = sum(v * p for v, p in zip(list(zip(*random_values))[0], probabilities))
+    expected_mean = sum(v * p for v, p in zip(choice_mean, choice_prob))
 
-    expected_var = (((-2 - expected_mean)**2*0.15 + (4 - expected_mean)**2*0.25
-                    + (10 - expected_mean)**2*0.35 + (15 - expected_mean)**2*0.25)
-                    + (2 * 0.15 + 1 * 0.25 + 3 * 0.35 + 2 * 0.25))
+    expected_var = (sum((mean - expected_mean)**2 * p  for mean, p in zip(choice_mean, choice_prob))
+        + sum((var * p) for var, p in zip(choice_mean, choice_prob)))
+
     print(expected_mean)
     print(expected_var)
 
-    bins = np.linspace(ceil(min(extracted_values)),
-                       floor(max(extracted_values)),
-                       40)  # fixed number of bins
+
+    print("COMPUTING...")
+
+
+    for _ in range(RANGE):
+        mu, sigma = choices(list(zip(choice_mean, choice_var)), choice_prob)[0]
+        vals.append(rng.normal(mu, sigma))
+    print(np.mean(vals))
+    print(np.var(vals))
+
+
     fig, ax = plt.subplots()
-    ax.hist(extracted_values, linewidth=0.5, edgecolor="white", bins=bins)
+    ax.hist(vals, linewidth=0.5, edgecolor="white", bins=40)
     plt.show()
 
 
