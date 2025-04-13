4803d201b3e4a84fb6095f93907a643a5b7f2081
Matteo Di Noia <matteodinoia02@gmail.com>
2025-04-13 11:38:41 +0200

    refactoring es 2.2

--- 02/exercise-02.py
+++ 02/exercise-02.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+PI = math.pi
+ETA95 = 1.96
 
 def weird_function(x: float) -> float:
-    return (x**2) * math.sin(x * math.pi)**2
-
+    return x**2 * math.sin(x * PI)**2
 
 def weird_integral(x: float) -> float:
-    return ((4 * math.pi ** 3 * x ** 3 - 6 * math.pi * x * math.cos(2 * math.pi * x)
-            + (3 - 6 * math.pi ** 2 * x ** 2) * math.sin(2 * math.pi * x))
-            / (24 * math.pi ** 3) + 4.424009112268247) /8.8480182
-
+    return ((4 * PI**3 * x**3 - 6 * PI * x * math.cos(2 * PI * x)
+            + (3 - 6 * PI**2 * x**2) * math.sin(2 * PI * x))
+            / (24 * PI**3) + 4.424009112268247) / 8.8480182
 
 def sampling_weird(a: float, b: float, upper: float) -> float:
     while True:
@@ -21,7 +20,6 @@ def sampling_weird(a: float, b: float, upper: float) -> float:
         u = float(np.random.uniform(0, upper, 1)[0])
         if u <= weird_function(x):
             return x
-
 
 def bootstrap_procedure(array, r0: int, gamma: float, st_func):
     r = math.ceil(2*r0/(1-gamma))-1
@@ -32,59 +30,61 @@ def bootstrap_procedure(array, r0: int, gamma: float, st_func):
     stat_calculated.sort()
     return stat_calculated[r0], stat_calculated[r + 1 - r0]
 
+def es3median(arr_sorted):
+    n = len(arr_sorted)
+    exact_median = np.median(arr_sorted)
+
+    low = math.floor(n / 2 - ETA95 / 2 * math.sqrt(n))
+    up = math.ceil(n / 2 + ETA95 / 2 * math.sqrt(n) + 1)
+    print("Median is", exact_median, "[", arr_sorted[low], ";", arr_sorted[up], "]")
+    print(bootstrap_procedure(arr_sorted, 25, 0.95, np.median))
+
+def es3mean(arr):
+    n = len(arr)
+    mean = float(np.mean(arr))
+    std_dev_mean = float(np.std(arr))
+    error_mean = ETA95 * std_dev_mean / math.sqrt(n)
+    print("Mean", mean, "+-", error_mean, "[", (mean - error_mean), ";", (mean + error_mean), "]")
+    print(bootstrap_procedure(arr, 25, 0.95, np.mean))
+
+def es3quantile(arr_sorted, p):
+    n = len(arr_sorted)
+    quant = np.quantile(arr_sorted, p)
+    low = math.floor(n * p - ETA95 * math.sqrt(n * p * (1 - p)))
+    up = math.ceil(n * p + ETA95 * math.sqrt(n * p * (1 - p)) + 1)
+    print("The", p, "quantile is", quant, "[", arr_sorted[low], ";", arr_sorted[up], "]")
+    print(bootstrap_procedure(arr_sorted, 25, 0.95, lambda arr: np.quantile(arr, p)))
+
+def es4(samples):
+    n = len(samples)
+    correct = 0
+    for subset in np.reshape(samples, (100, 200)):
+        mean = float(np.mean(subset))
+        std_dev = float(np.std(subset))
+        err = ETA95 * std_dev / math.sqrt(n)
+        if mean - err <= 0 <= mean + err:
+            correct += 1
+    print("True mean is in interval ", correct, "% of the times")
 
 def main():
-    #### Punti 1 e 2
+    #### Subpoint 1
     y_upper_bound = 6.36 # obtained with WolframAlpha
     samples = []
     for _ in range(20000):
         samples.append(sampling_weird(-3, 3, y_upper_bound))
 
-    #### Punto 3
-    n = 200
-    sub_sample = samples[:n]
-    print("Over 200 cases:")
-
-    #### Punto 3 Median
+    #### Subpoint 3
+    sub_sample = samples[:200]
     sub_sample.sort()
-    exact_median = np.median(sub_sample)
-    eta = 1.96
-    low = math.floor(n/2 - eta/2 * math.sqrt(n))
-    up = math.ceil(n/2 + eta/2 * math.sqrt(n) + 1)
-    print("Median", exact_median, "[", sub_sample[low], ";", sub_sample[up], "]")
-    print(bootstrap_procedure(sub_sample, 25, 0.95, np.median))
+    print("Over 200 cases:")
+    es3median(sub_sample)
+    es3mean(sub_sample)
+    es3quantile(sub_sample, 0.9)
 
-    #### Punto 3 Median
-    quant_90 = np.quantile(sub_sample, 0.9)
-    low90 = math.floor(n * 0.9 - eta * math.sqrt(n * 0.9 * (1 - 0.9)))
-    up90 = math.ceil(n * 0.9 + eta/2 * math.sqrt(n * 0.9 * (1 - 0.9)) + 1)
-    print("Median", quant_90, "[", sub_sample[low90], ";", sub_sample[up90], "]")
-    print(bootstrap_procedure(sub_sample, 25, 0.95, lambda arr: np.quantile(arr, 0.9)))
+    ### Subpoint 4
+    es4(samples)
 
-    #### Punto 3 Mean
-    mean = float(np.mean(sub_sample))
-    std_dev_mean = float(np.std(sub_sample))
-    error_mean = eta * std_dev_mean / math.sqrt(n)
-    #quant_90 = np.quantile(sub_sample, 0.9)
-    #low90 = math.floor(n * 0.9 - eta * math.sqrt(n * 0.9 * (1 - 0.9)))
-    #up90 = math.ceil(n * 0.9 + eta/2 * math.sqrt(n * 0.9 * (1 - 0.9)) + 1)
-    print("Mean", mean, "+-", error_mean, "[", (mean - error_mean), ";", (mean + error_mean), "]")
-    print(bootstrap_procedure(sub_sample, 25, 0.95, np.mean))
-
-    ### Punto 4
-    correct200 = 0
-    for set200 in np.reshape(samples, (100, 200)):
-        mean200 = float(np.mean(set200))
-        std_dev_mean_200 = float(np.std(set200))
-        error_mean200 = eta * std_dev_mean_200 / math.sqrt(n)
-        low_mean200 = mean200 - error_mean200
-        high_mean200 = mean200 + error_mean200
-        if low_mean200 <= 0 <= high_mean200:
-            correct200 += 1
-    print("True mean is in interval ", correct200, "% of the times")
-
-
-    ### Plot
+    ### Subpoint 2
     x = np.linspace(-3, 3, 1000)
     y = []
     for v in x:
