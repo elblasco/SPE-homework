e679b782a4ca3650340dbe7c508ca579b73e07c6
elblasco <alessio.blascovich@studenti.unitn.it>
2025-05-04 14:34:55 +0200

    add: least squared

--- 03/exercise-01.py
+++ 03/exercise-01.py
@@ -2,18 +2,49 @@
 
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
+def point1(measurements: list, times: list):
+    fig, ax = plt.subplots()
+    ax.scatter(times, measurements)
+    plt.show()
+
+def sqrt(times: list, measurements: list, m: int):
+    y: list = measurements
+    x: list = times
+    A: list = []
+    
+    for entry in x:
+        A.append([entry**i for i in range(m+1)])
+
+    AT: list = np.transpose(A)
+
+    inv_mul= np.linalg.inv(np.matmul(AT, A))
+
+    b: list = np.matmul(np.matmul(inv_mul, AT), y)
+    
+    w = np.linspace(0,10,100)
+    z = [np.polyval(np.flip(b), i) for i in w]
+    fig, ax = plt.subplots()
+    ax.plot(w,z)
+    plt.show()
+    print(b)
+    
 def main():
     csvFile = csv.reader(open('data_ex1_wt.csv', mode ='r'))
     measurements: list = []
     times: list = []
     for line in csvFile:
-        times.append(line[0])
-        measurements.append(line[1])
+        times.append(float(line[0]))
+        measurements.append(float(line[1]))
 
-    fig, ax = plt.subplots()
-    ax.scatter(times, measurements)
-    plt.show()
+    #point1(measurements, times)
+
+    #for i in range(10):
+    sqrt(times, measurements, 6)
+
+    #w.r.t. Slides y = measurements and x = times
+
     
 if __name__ == "__main__":
     main()
