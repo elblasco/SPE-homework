e1ff3c5944cbfa74a497fe1ac7e26701c8d34711
elblasco <alessio.blascovich@studenti.unitn.it>
2025-05-09 14:48:50 +0200

    add: finished python file

--- 03/exercise-01.py
+++ 03/exercise-01.py
@@ -4,10 +4,7 @@ import csv
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-
-def point1(measurements: list, times: list, ax):
-    ax[0].scatter(times, measurements, label='Scatterplot', color='grey')
-
+    
 def least_square(times: list, measurements: list, m: int):
     y: list = measurements
     x: list = times
@@ -27,39 +24,54 @@ def least_square(times: list, measurements: list, m: int):
 def norm_pdf(x, mu, var):
     return (1/(math.sqrt(2 * math.pi * var))) * math.exp(- (((x-mu)**2)/(2 * var)))
 
-def exp_max(X: list, mus: list, var: list):
+# This function returns the pre-compited values of exp_max
+# since it takes a long time to compute
+def exp_max_prec(X: list, mus: list, var: list, ITERATION: int):
+    match len(mus):
+        case 2:
+            mus = [-3.07, 4.04]
+            var = [7.63, 1.39]
+            p_colours = [0.568, 0.432]
+        case 3:
+            mus = [-4.72, 0.410, 4.26]
+            var = [3.07, 5.96, 0.980]
+            p_colours = [0.345, 0.302, 0.353]
+        case 4:
+            mus = [-4.78, 0.923, -0.812, 4.26]
+            var = [2.96, 5.08, 7.94, 0.979]
+            p_colours = [0.319, 0.167, 0.162, 0.352]
+    return(mus, var, p_colours)
+
+def exp_max(X: list, mus: list, var: list, ITERATION: int):
     if len(mus) != len(var):
         return
     n: int = len(mus)
     prob_color = [1/n for c in range(n)]
-    for _ in range(300):
+    for _ in range(ITERATION):
         prob_colour_xi: list = [[] for c in range(n)]
-        for i in range(len(X)):
-            prob_xi_colour: list = []
-            for c in range(n):
-                prob_xi_colour.append(norm_pdf(X[i], mus[c], var[c]))
+        for x in X:
+            prob_xi_colour: list = [
+                norm_pdf(x, mu, var) * p_colour
+                for mu, var, p_colour in zip(mus, var, prob_color)
+            ]
 
-            denominator: float = sum(
-                prob_xi_colour[c] * prob_color[c] for c in range(n)
-            )
+            denominator: float = sum(prob_xi_colour)
             
             for c in range(n):
-                prob_colour_xi[c].append(prob_xi_colour[c] * prob_color[c] / denominator)
+                prob_colour_xi[c].append(prob_xi_colour[c] / denominator)
 
         for c in range(n):
-            denominator = sum(
-                prob_colour_xi[c][i]
-                for i in range(len(X))
-            )
+            prob_colour_fixed_x: list = prob_colour_xi[c] 
+            denominator = sum(prob_colour_fixed_x)
             
             mus[c] = sum(
-                prob_colour_xi[c][i] * X[i]
-                for i in range(len(X))
+                x * prob
+                for x, prob in zip(X, prob_colour_fixed_x)
             ) / denominator
 
             var[c] = sum(
-                prob_colour_xi[c][i] * (X[i] - mus[c])**2
-                for i in range(len(X))
+                prob * (x - mus[c])**2
+                for x, prob in zip(X, prob_colour_fixed_x)
             ) / denominator
             
             prob_color[c] = denominator / len(X)
@@ -67,6 +79,71 @@ def exp_max(X: list, mus: list, var: list):
     return(mus, var, prob_color)
 
 
+def plot_exp_max(measurements: list, mus: list, variances: list, p_colours: list, ax):
+    w = np.linspace(min(measurements), max(measurements), 1000)
+    z = [
+        sum(
+            norm_pdf(x, mus[i], variances[i]) * p_colours[i]
+            for i in range(len(mus))
+        )
+        for x in w
+    ]
+    ax.plot(w, z, label=f'Expectation maximization n={len(mus)}')
+    ax.legend(loc=2, prop={'size': 10})
+    return z
+
+
+def point2(measurements: list, times: list, old_ax, new_ax):
+    for rank in range(1, 7):
+         trend: list = least_square(times, measurements, rank)
+         w = np.linspace(0,2,100)
+         z = [np.polyval(trend, i) for i in w]
+         old_ax.plot(w, z, label = f"lstsqr - {rank}")
+    
+    old_ax.legend()
+    old_ax.legend(loc=3, prop={'size': 10})
+
+    trend: list = least_square(times, measurements, 5)
+    for i in range(len(measurements)):
+         measurements[i] = measurements[i] - np.polyval(trend, times[i])
+
+    new_ax.scatter(times, measurements, label='De-trended scatterplot')
+    new_ax.legend(loc=3, prop={'size': 10})
+
+def point4(measurements: list, times: list, ax):
+    ax.hist(measurements, bins=200, density=True, label='De-trended histogram')
+    mus, variances, p_colours = exp_max_prec(measurements, [-1,0,1], [1, 1, 1], 300)
+    plot_exp_max(measurements, mus, variances, p_colours, ax)
+
+def point5(measurements: list, times: list, ax):
+    ax.hist(measurements, bins=200, density=True, label='De-trended histogram')
+    MIN_V = -10
+    MAX_V = 10
+    INTERVAL = 50
+    STEP = (MAX_V - MIN_V) / INTERVAL
+    n_i = [0 for _ in range(INTERVAL)]
+    for measurement in measurements:
+        i = min(INTERVAL - 1, max( math.floor((measurement - MIN_V) / (MAX_V - MIN_V) * INTERVAL), 0))
+        n_i[i] += 1 
+    for i in range(2, 5):
+        
+        mus, variances, p_colours = exp_max_prec(measurements, [0] * i, [1] * i, 150)
+        # print(mus, variances, p_colours)
+        plot_exp_max(measurements, mus, variances, p_colours, ax)
+        p_i = [
+            sum(
+                norm_pdf(x + STEP / 2, mus[i], variances[i]) * p_colours[i]
+                for i in range(len(mus))
+            ) * ((MAX_V - MIN_V) / INTERVAL)
+            for x in np.linspace(MIN_V, MAX_V, INTERVAL, endpoint = False)
+        ]
+
+        T = sum(
+            (n_i[k] - len(measurements) * p_i[k])**2/ (len(measurements) * p_i[k])
+            for k in range(INTERVAL)
+        )
+        print("T =",T, "with i=", i)
+        
 def main():
     csvFile = csv.reader(open('data_ex1_wt.csv', mode ='r'))
     measurements: list = []
@@ -74,43 +151,33 @@ def main():
     for line in csvFile:
         times.append(float(line[0]))
         measurements.append(float(line[1]))
-    fig, ax = plt.subplots(1, 2)
-    plt.xticks(size = 22)
-    plt.yticks(size = 22)
-    point1(measurements, times, ax)
+         
+    # POINT 1
+    fig1, ax1 = plt.subplots(1, 1)
+    ax1.scatter(times, measurements, label='Scatterplot', color='grey')
+    
+    # POINT 2
+    fig2, ax2 = plt.subplots(1, 1)
+    point2(measurements, times, ax1, ax2)
+    fig1.savefig('./report/img/point1.png', bbox_inches='tight')
+    fig2.savefig('./report/img/point2.png', bbox_inches='tight')
+    
+    # POINT 3
+    fig3, ax3 = plt.subplots(1, 1)
+    ax3.hist(measurements, bins=200, density=True, label='De-trended histogram')
+    fig3.savefig('./report/img/point3.png', bbox_inches='tight')
+    
+    # POINT 4
+    fig4, ax4 = plt.subplots(1, 1)
+    point4(measurements, times, ax4)
+    fig4.savefig('./report/img/point4.png', bbox_inches='tight')
 
-    for rank in range(1, 7):
-         trend: list = least_square(times, measurements, rank)
-         w = np.linspace(0,2,100)
-         z = [np.polyval(trend, i) for i in w]
-         ax[0].plot(w, z, label = f"lstsqr - {rank}")
-    
-    ax[0].legend()
-    ax[0].legend(loc=3, prop={'size': 14})
-    trend: list = least_square(times, measurements, 5)
-    for i in range(len(measurements)):
-         measurements[i] = measurements[i] - np.polyval(trend, times[i])
-    ax[1].scatter(times, measurements, label='De-trended scatterplot')
-    ax[1].legend(loc=3, prop={'size': 14})
-    # #mus, variances, p_colours = exp_max(measurements, [-1,0,1], [1, 1, 1])
-    # mus = [-4.72, 0.41, 4.26]
-    # variances = [3.07, 5.96, 0.98]
-    # p_colours = [0.345, 0.302, 0.353]
-    
-    # ax[1].hist(measurements, bins=200, density=True)
-    # w = np.linspace(min(measurements), max(measurements), 1000)
-    # z = [
-    #     sum(
-    #         norm_pdf(x, mus[i], variances[i]) * p_colours[i]
-    #         for i in range(len(mus))
-    #     )
-    #     for x in w
-    # ]
-    # ax[1].plot(w, z)
-    plt.show()
+    # POINT 5
+    fig5, ax5 = plt.subplots(1, 1)
+    point5(measurements, times, ax5)
+    fig5.savefig('./report/img/point5.png', bbox_inches='tight')
 
-    
-    
+    print("plots have been saved as images")
     
 if __name__ == "__main__":
     main()
