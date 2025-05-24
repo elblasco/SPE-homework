f05aa784780ddf2b65f24b2a2771fb80220b7961
Matteo Di Noia <dmatteo002@gmail.com>
2025-05-24 18:43:13 +0200

    format and remane enum

--- 04/exercise-01.py
+++ 04/exercise-01.py
@@ -5,20 +5,22 @@ import heapq
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Event(Enum):
+
+class EventType(Enum):
     START = 0
     ARRIVAL = 1
     DEPARTURE = 2
     END = 3
 
+
 def generate_events(start_time, end_time, lamb) -> list:
     times: list = np.random.uniform(
-        start_time,end_time, int(lamb * (end_time - start_time))
+        start_time, end_time, int(lamb * (end_time - start_time))
     )
-    events: list = list(zip(times, [Event.ARRIVAL for _ in range(len(times))]))
+    events: list = list(zip(times, [EventType.ARRIVAL for _ in range(len(times))]))
 
-    events.append((start_time, Event.START))
-    events.append((end_time, Event.END))
+    events.append((start_time, EventType.START))
+    events.append((end_time, EventType.END))
     return events
 
 
@@ -27,51 +29,57 @@ def simulation(start_time, end_time, lamb, mi) -> (list, list):
     packets: int = 0
     events_heap: list = generate_events(start_time, end_time, lamb)
     heapq.heapify(events_heap)
-    packets_per_istant:list = []
+    packets_per_istant: list = []
     istants: list = []
     while len(events_heap) > 0:
         current_time, current_event = heapq.heappop(events_heap)
-        #print(current_event)
+        # print(current_event)
         match current_event:
-            case Event.START:
+            case EventType.START:
                 print("Started")
-            case Event.ARRIVAL:
-                #packets += 1
+            case EventType.ARRIVAL:
+                # packets += 1
                 if not server_busy:
                     server_busy = True
-                    schduled_departure = np.random.exponential(1/mi) + current_time
-                    heapq.heappush(events_heap, (schduled_departure, Event.DEPARTURE))
+                    schduled_departure = np.random.exponential(1 / mi) + current_time
+                    heapq.heappush(
+                        events_heap, (schduled_departure, EventType.DEPARTURE)
+                    )
                 else:
                     packets += 1
 
-            case Event.DEPARTURE:
+            case EventType.DEPARTURE:
                 if packets > 0:
                     packets -= 1
-#                    packets_per_istant[event_idx] = packets
+                    #                    packets_per_istant[event_idx] = packets
                     # Schudeled departure for the nextr waiting packet
-                    schduled_departure = np.random.exponential(1/mi) + current_time
-                    heapq.heappush(events_heap, (schduled_departure, Event.DEPARTURE))
+                    schduled_departure = np.random.exponential(1 / mi) + current_time
+                    heapq.heappush(
+                        events_heap, (schduled_departure, EventType.DEPARTURE)
+                    )
                 else:
                     server_busy = False
-            case Event.END:
+            case EventType.END:
                 break
         packets_per_istant.append(packets + (1 if server_busy else 0))
         istants.append(current_time)
     return packets_per_istant, istants
 
+
 def main():
     totali_pacchetti: list = []
-    
+
     for _ in range(100):
         pacchetti, istanti = simulation(0, 1e4, 10, 2)
         totali_pacchetti += pacchetti
-    #fig, ax = plt.subplots(1, 2)
-    #ax[0].plot(istanti, pacchetti)
+    # fig, ax = plt.subplots(1, 2)
+    # ax[0].plot(istanti, pacchetti)
     d = np.diff(np.unique(totali_pacchetti)).min()
-    left_of_first_bin = min(totali_pacchetti) - float(d)/2
-    right_of_last_bin = max(totali_pacchetti) + float(d)/2
+    left_of_first_bin = min(totali_pacchetti) - float(d) / 2
+    right_of_last_bin = max(totali_pacchetti) + float(d) / 2
     plt.hist(totali_pacchetti, np.arange(left_of_first_bin, right_of_last_bin + d, d))
     plt.show()
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
