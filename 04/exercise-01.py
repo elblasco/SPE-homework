44bc467d42a874eedbd73930400e3aaa9f167e55
Matteo Di Noia <dmatteo002@gmail.com>
2025-06-01 12:49:11 +0200

    report code

--- 04/exercise-01.py
+++ 04/exercise-01.py
@@ -58,7 +58,7 @@ def get_line(xs: list[float], ys: list[float]):
 
     # r-squared
     r2 = 1 - (ss_res / ss_tot)
-    print("Value of r2 of line is", r2)
+    print("Value of r2 of line", line[0], "x + ", line[1], "is", r2)
     return line
 
 
@@ -116,13 +116,15 @@ def plot_distribution_packets(ax1, ax2, start, end, n_packets, instants):
             math.e ** (empirical_log_line[1] + empirical_log_line[0] * x)
             for x in linspace_n_packet_in_queue
         ],
-        label="Theoretical values",
+        label="Theoretical distribution",
     )
     ax1.plot(
         n_packet_in_queue,
         n_packet_in_queue_occur,
-        label="Sorted unique numbers of packets",
+        label="Empirical distribution",
     )
+    ax1.set_xlabel("packets")
+    ax1.set_ylabel("time")
     ax1.legend(loc="upper right")
 
     ax2.plot(
@@ -131,30 +133,35 @@ def plot_distribution_packets(ax1, ax2, start, end, n_packets, instants):
             empirical_log_line[1] + empirical_log_line[0] * c
             for c in range(len(n_packet_in_queue))
         ],
-        label="Theoretical logarithm of packets in queue",
+        label="Theoretical distribution",
     )
     ax2.plot(
         n_packet_in_queue,
         empirical_log_n_packets,
-        label="Empirical logarithm of packets in queue",
+        label="Empirical distribution",
     )
+    ax2.set_xlabel("packets")
+    ax2.set_ylabel("log time")
     ax2.legend(loc="upper right")
 
 
 def ex1(lam, mi, sim_len, n_simulation):
     start = 0
-    end = sim_len / 10
+    end = sim_len
 
     mean_ith_simulation: list = []
     avg_packet: list = []
     instant_avg: list = []
     n_packets: list = []
     instants: list = []
+    ro = lam / mi
+    # Enable warmup to discard first datapoints (by replacing 0)
+    warmup = 10 * math.ceil(ro / (1 - ro))
 
     # Start simulation
     for sim in range(n_simulation):
         print(f"Start simulation {sim}")
-        server: QueueServer = QueueServer(start, end, lam, mi)
+        server: QueueServer = QueueServer(-warmup, end, lam, mi)
         stats = server.simulate(
             lambda s, time, event: [
                 (
@@ -165,7 +172,8 @@ def ex1(lam, mi, sim_len, n_simulation):
                 )
             ]
         )
-        n_packets, instants = map(lambda x: list(x), zip(*stats))
+        filtered = list(filter((lambda el: el[1] >= (-warmup / 2)), stats))
+        n_packets, instants = map(lambda x: list(x), zip(*filtered))
 
         mean_i = mean_time_weighted(n_packets, instants, start, end)
         mean_ith_simulation.append(float(mean_i))
@@ -175,6 +183,8 @@ def ex1(lam, mi, sim_len, n_simulation):
         )
 
     avg_packet = [a / n_simulation for a in avg_packet]
+    avg = list(filter((lambda el: el[1] >= 0), zip(avg_packet, instant_avg)))
+    avg_packet, instant_avg = map(lambda x: list(x), zip(*avg))
 
     print("Preparing graphs (may take some time)")
     _, ax = plt.subplots(2, 2)
@@ -237,17 +247,18 @@ def time_based_overlapping_batch_mean(
     step_size = (end - start - batch_time_size) / (batch_number - 1)
     window_start = window_end = 0
     old_n_packet = 0
+    print(step_size)
 
     for batch in range(batch_number):
         batch_start: float = start + (step_size * batch)
         batch_end: float = batch_start + batch_time_size
-        while instants[window_start] < batch_start:
+        while window_start < len(instants) and instants[window_start] < batch_start:
             old_n_packet = n_packets[window_start]
             window_start += 1
 
         window_end = max(window_start, window_end)
 
-        while instants[window_end] < batch_end:
+        while window_end < len(instants) and instants[window_end] < batch_end:
             window_end += 1
 
         mean_ith_batch.append(
@@ -280,7 +291,12 @@ def test_overlapping_batch_means(lam: float, mi: float, sim_time_len: int):
     n_packets, instants = map(lambda x: list(x), zip(*packets_and_instants))
 
     mean_ith_batch = time_based_overlapping_batch_mean(
-        n_packets, instants, 1_000, 10_001, simulation_start, simulation_end
+        n_packets,
+        instants,
+        int(sim_time_len / 1000),
+        10000,
+        simulation_start,
+        simulation_end,
     )
 
     grand_mean = float(np.average(mean_ith_batch))
@@ -296,9 +312,11 @@ def test_overlapping_batch_means(lam: float, mi: float, sim_time_len: int):
         n_packets, instants, simulation_start, simulation_end
     )
 
+    ro = lam / mi
+    theoretical_mean_n_packets = ro / (1 - ro)
     print(
-        "Batch Overlapping Means method, expected mean",
-        expected_grand_mean,
+        "Batch Overlapping Means method, theorical mean",
+        theoretical_mean_n_packets,
         "empirical mean",
         grand_mean,
         "+-",
@@ -321,8 +339,8 @@ def test_overlapping_batch_means(lam: float, mi: float, sim_time_len: int):
 
 def main():
     sim_time_len = 50_000
-    ro = 1 / 10  # lab/ mi
-    n_simulation = 10
+    ro = 2 / 3  # lab/ mi
+    n_simulation = 50
 
     lam = 1  # arrivals
     mi = lam / ro  # departures
