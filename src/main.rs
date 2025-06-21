2e3a192cb7dea51a75d11b4af76957981f7da351
elblasco <alessio.blascovich@studenti.unitn.it>
2025-06-21 10:24:51 +0200

    add: pretty print

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@
 #![warn(clippy::cargo)]
 #![allow(clippy::cargo_common_metadata)]
 #![allow(dead_code)]
+#![allow(clippy::missing_const_for_fn)]
 
-use crate::simulation::Simulation;
+use crate::simulation::{EventKind, Simulation};
 use crate::train_lines::train_line::{Direction, TrainLine};
 
 mod graph;
@@ -12,7 +13,6 @@ mod simulation;
 mod train_lines;
 
 fn main() {
-    println!("Hello, world!");
     let mut system = Simulation::new(0, 1000);
     let line = TrainLine::new(vec![1, 2, 3, 4, 5]);
     let line = system.add_line(line);
@@ -20,4 +20,61 @@ fn main() {
     system.add_train(5, &line, 3, Direction::Right).unwrap();
 
     simulate(system);
+}
+
+fn simulate(mut system: Simulation) {
+    let mut running = true;
+    let mut i = 0;
+    while running {
+        println!("\nEVENT {i} {:?}:", system.peek_event());
+        let current_event = system.peek_event().unwrap();
+        match current_event.kind {
+            EventKind::Start | EventKind::End => {}
+            EventKind::TrainArrive(train_id) => {
+                let train = system
+                    .trains
+                    .get(&train_id)
+                    .expect("Arrival of a train that doesn't exist");
+
+                let start = train.get_curr_station();
+                let (end, dir) = train.get_next_station();
+                println!(
+                    "{train_id} train {train:?} entering station {end} (from {start} with direction {dir:?}"
+                );
+            }
+            EventKind::TrainDepart(train_id) => {
+                let train = system
+                    .trains
+                    .get(&train_id)
+                    .expect("Departure of a train that doesn't exist");
+
+                let start = train.get_curr_station();
+                let (end, dir) = train.get_next_station();
+                println!(
+                    "{train_id} train {train:?} exiting station {start} (to {end}) with direction {dir:?}"
+                );
+            }
+            EventKind::PersonArrive(station_id) => {
+                let station = system
+                    .graph
+                    .get_node(station_id)
+                    .expect("Person arrive to not existent station");
+
+                println!("Person arrived ad station {station_id}: {station:?}");
+            }
+        }
+
+        match system.simulation_step() {
+            Ok(status) => running = !status,
+            Err(error) => {
+                println!("Simulation errored out");
+                println!("{error}");
+                println!();
+                println!("{system:?}");
+                running = false;
+            }
+        }
+
+        i += 1;
+    }
 }
