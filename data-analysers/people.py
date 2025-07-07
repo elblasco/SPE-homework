7c020d5f8779c14b841cd399cfbca9e4c078afd2
elblasco <alessio.blascovich@studenti.unitn.it>
2025-07-07 10:11:23 +0200

    feat(people.py): different lines

--- data-analysers/people.py
+++ data-analysers/people.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env pyton3
 
-import matplotlib.pyplot as plt
 import csv
 
-csv_reader = csv.reader(open('../output/people.csv', mode ='r'))
+import matplotlib.pyplot as plt
+
+csv_reader = csv.reader(open('../output/people.csv', mode='r'))
 
 # time_instant: list = []
 # people: list = []
 times: list = []
-stations: dict = []
+lines: list = []
 people: list = []
 
 next(csv_reader)
@@ -16,15 +17,42 @@ next(csv_reader)
 for line in csv_reader:
     times.append(float(line[0]))
     people.append(float(line[1]))
-    stations.append(line[2].strip())
+    lines.append(line[2].strip())
 
-totals, times = zip(*[(p, t) for p, t,  station in zip(people, times,  stations) if station == "All lines"])
+# totals = []
+# times = []
+# filtered_U1 = []
+# times_U1 = []
+# filtered_U2 = []
+# times_U2 = []
+# filtered_U3 = []
+# times_U3 = []
+# filtered_U4 = []
+# times_U4 = []
+# filtered_U6 = []
+# times_U6 = []
+#
+# for time, n, line in zip(times, people, lines):
+totals, times = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "All lines"])
+filtered_U1, times_U1 = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "U1"])
+filtered_U2, times_U2 = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "U2"])
+filtered_U3, times_U3 = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "U3"])
+filtered_U4, times_U4 = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "U4"])
+filtered_U6, times_U6 = zip(*[(p, t) for p, t, line in zip(people, times, lines) if line == "U6"])
 
+_, ax = plt.subplots(2, 6)
 
-print(len(totals))
+ax[0][0].hist(totals, bins=20)
+ax[0][1].hist(filtered_U1, bins=20)
+ax[0][2].hist(filtered_U2, bins=20)
+ax[0][3].hist(filtered_U3, bins=20)
+ax[0][4].hist(filtered_U4, bins=20)
+ax[0][5].hist(filtered_U6, bins=20)
+ax[1][0].plot(times, totals, label='Total')
+ax[1][1].plot(times_U1, filtered_U1, label='Line U1')
+ax[1][2].plot(times_U2, filtered_U2, label='Line U2')
+ax[1][3].plot(times_U3, filtered_U3, label='Line U3')
+ax[1][4].plot(times_U4, filtered_U4, label='Line U4')
+ax[1][5].plot(times_U6, filtered_U6, label='Line U6')
 
-_, ax = plt.subplots(1, 2)
-
-ax[0].hist(totals, bins=20)
-ax[1].plot(times, totals)
 plt.show()
