df47b38901d497141dbee4954a0ea47a8e6f320f
elblasco <alessio.blascovich@studenti.unitn.it>
2025-07-22 14:36:39 +0200

    add: const file

---
+++ src/utils/config.rs
@@ -0,0 +1,20 @@
+use crate::train_lines::Time;
+use crate::utils::time::{from_days, from_hour, from_minutes, from_seconds};
+
+pub const AVG_SPEED_M_S: f64 = 30.0 / 3.6;
+pub const MAX_SPEED_M_S: f64 = 50.0 / 3.6;
+pub const MIN_SPEED_M_S: f64 = 10.0 / 3.6;
+
+pub const EDGE_MAX_CAPACITY: usize = 1;
+
+pub const TIME_BETWEEN_SNAPSHOT: Time = from_seconds(5.0);
+pub const TIME_TO_RECOVER: Time = from_minutes(30.0);
+pub const TIME_AT_STATION: Time = from_seconds(20.0);
+pub const KM_BEFORE_CRASHING: f64 = 10.0;
+
+pub const WARMUP_TIME: Time = from_days(10.0);
+pub const SIM_LEN: Time = from_days(20.0);
+pub const PRINT_INTERVAL: Time = from_hour(6.0);
+
+pub const TRAIN_CAPACITY: usize = 800;
+pub const TRAIN_PER_LINE: usize = 10;
